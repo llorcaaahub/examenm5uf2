9a9abddb71f52a0c76d78b63264fed1df2dc0a20
llorcaaahub <98091174+llorcaaahub@users.noreply.github.com>
2023-03-13 08:34:37 +0100

    Posar 2 Botons i canviar el nom del form

--- projecteC/ProjecteCProjectem5uf2/Program.cs
+++ projecteC/ProjecteCProjectem5uf2/Program.cs
@@ -16,7 +16,7 @@ namespace ProjecteCProjectem5uf2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmMain());
         }
     }
 }
